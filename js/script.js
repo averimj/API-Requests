e85ef1990dbb39e549be0343e6e62de2132bb9b9
Averi Johnson <averimj@live.com>
2020-02-05 15:38:43 -0700

    add helper functions

--- js/script.js
+++ js/script.js
@@ -2,9 +2,15 @@
 const search = document.getElementsByClassName('search-container');
 const gallery = document.getElementById('gallery');
 
+const card = document.createElement('div');
+card.className = 'card';
+
+const modal = document.createElement('div')
+modal.className = 'modal-container';
+
 // FETCH FUNCTIONS
 
-// fetching data, checks the status, converts the response into json and sends an error message is need be
+// fetching data, checks the status, converts the response into json and sends an error message if need be
 function fetchData(url) {
   return fetch(url)
     .then(checkStatus)
@@ -12,26 +18,85 @@ function fetchData(url) {
     .catch( error => console.log('Houston, we have a problem!', error) )
 }
 
-// promise.all is used when you have 2 or more api's to fetch ... wanted to use anyway
-Promise.all([
-
-  // returns 12 users with US nationality, picture, name, email, location, dob & phone
+// returns 12 users with US nationality, picture, name, email, location, dob & phone
  fetchData('https://randomuser.me/api/?nat=us&inc=picture,name,email,location,dob,phone&results=12')
-])
-
-// takes the data we get back a generates a "employeeContainer" and a modalContainer
-.then(data => {
-   data[0].results.map(profile => generateEmployeeCard(profile));
-   data[0].results.map(modalProfile => generateEmployeeModal(modalProfile));
-
-  // const employeeProfile = data[0].results.map(profile => generateEmployeeCard() );
-  // const modal = data[0].results.map(modalProfile => generateEmployeeModal() );
-
-  generateEmployeeCard(employeeProfile);
-  generateEmployeeModal(modal);
-})
+  .then(data => data.results)
 
 // HELPER FUNCTIONS
+
+
+// creates the search box
+function generateForm() {
+  const form = `
+    <form action='#' method='get'>
+      <input type='search' id='search-input' class='search-input' placeholder='Search...'>
+      <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+  </form>
+  `;
+  // search.appendChild(form);
+  search.innerHTMl = form;
+}
+console.log(search);
+
+
+// creates the card with employee pictures
+function generateCardContainer() {
+  const imageContainer = `
+    <div class="card-img-container">
+        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+    </div>
+  `;
+}
+
+
+// creates the card with employee basic information
+function generateInfoContainer() {
+  const infoContainer = `
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">first last</h3>
+        <p class="card-text">email</p>
+        <p class="card-text cap">city, state</p>
+    </div>
+  `;
+}
+
+
+// creates the modal with all employee information (dob, phone, address etc.)
+function generateModal() {
+  const modalContainer = `
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+      </div>
+    </div>
+  `;
+}
+
+
+// allows user to navigate to the previous and next employee modal
+function generateModalButtons() {
+ const modButton = `
+   <div class="modal-btn-container">
+       <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+       <button type="button" id="modal-next" class="modal-next btn">Next</button>
+   </div>
+ `;
+}
+
+
+// puts the dob in stanard mm/dd/yyyy format
+function birthday() {
+
+}
+
 
 // ensures the response we get back is 200, if not gives us an error
 function checkStatus(response) {
@@ -41,55 +106,3 @@ function checkStatus(response) {
     return Promise.reject( new Error(response.statusText) );
   }
 }
-
-// creates the cardDiv--which holds each employee's basic information
-function generateEmployeeCard(data) {
- const cardDiv = document.createElement('div');
- cardDiv.setAttribute('class', 'card');
-
- const employeeContainer = `
-    <div class='card-img-container'>
-     <img class='card-img' src='${data.picture.large}' alt='profile picture'>
-    </div>
-    <div class='card-info-container'>
-      <h3 id='name' class='card-name cap'>${data.name.first} ${data.name.last}</h3>
-      <p class='card-text'>${data.email}</p>
-      <p class='card-text cap'>${data.location.city}, ${data.location.state}</p>
-    </div>
- `;
- cardDiv.innerHTML = employeeContainer;
-
- // appends cardDiv inside the gallery div
- gallery.appendChild(cardDiv);
-}
-
-
-// creates the modalContainer div--which when clicked, displays more information about the employee
-
-function generateEmployeeModal(data) {
- const modalContainer = document.createElement('div');
- modalContainer.setAttribute('class', 'modal-container');
-
- const modal = `
-  <div class='modal'>
-    <button type='button' id='modal-close-btn' class='modal-close-btn'><strong>X</strong></button>
-    <div class='modal-info-container'>
-        <img class='modal-img' src="${data.picture.large}" alt='profile picture'>
-        <h3 id='name' class='modal-name cap'>${data.name.first} ${data.name.last}</h3>
-        <p class='modal-text'>${data.email}</p>
-        <p class='modal-text cap'>${data.location.city}</p>
-        <hr>
-        <p class='modal-text'>${data.phone}</p>
-        <p class='modal-text'>${data.location.street.number} ${data.location.street.name}., ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
-        <p class='modal-text'>${data.dob.date}</p>
-    </div>
-  </div>
-  `;
-
- modalContainer.innerHTML = modal;
-
- // appends modalContainer div after the gallery div
- gallery.parentNode.insertBefore(modalContainer, gallery.nextSibling)
-}
-
-// EVENT LISTENERS
