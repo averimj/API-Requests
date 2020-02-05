fac5d239273387630ce2a9263477c9ee662b4552
Averi Johnson <averimj@live.com>
2020-02-05 16:12:45 -0700

    add single parentheses

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 
-const search = document.getElementsByClassName('search-container');
+const search = document.getElementsByClassName('div search-container');
 const gallery = document.getElementById('gallery');
 
+// creates the card-div
 const card = document.createElement('div');
 card.className = 'card';
 
+// creates the modal-div
 const modal = document.createElement('div')
 modal.className = 'modal-container';
+
+
 
 // FETCH FUNCTIONS
 
@@ -30,20 +34,19 @@ function generateForm() {
   const form = `
     <form action='#' method='get'>
       <input type='search' id='search-input' class='search-input' placeholder='Search...'>
-      <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+      <input type='submit' value='&#x1F50D;' id='search-submit' class='search-submit'>
   </form>
   `;
   // search.appendChild(form);
   search.innerHTMl = form;
 }
-console.log(search);
 
 
 // creates the card with employee pictures
 function generateCardContainer() {
   const imageContainer = `
-    <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+    <div class='card-img-container'>
+        <img class='card-img' src='https://placehold.it/90x90' alt='profile picture'>
     </div>
   `;
 }
@@ -52,10 +55,10 @@ function generateCardContainer() {
 // creates the card with employee basic information
 function generateInfoContainer() {
   const infoContainer = `
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
+    <div class='card-info-container'>
+        <h3 id='name' class='card-name cap'>first last</h3>
+        <p class='card-text'>email</p>
+        <p class='card-text cap'>city, state</p>
     </div>
   `;
 }
@@ -64,17 +67,17 @@ function generateInfoContainer() {
 // creates the modal with all employee information (dob, phone, address etc.)
 function generateModal() {
   const modalContainer = `
-    <div class="modal">
-      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-      <div class="modal-info-container">
-        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-        <h3 id="name" class="modal-name cap">name</h3>
-        <p class="modal-text">email</p>
-        <p class="modal-text cap">city</p>
+    <div class='modal'>
+      <button type='button' id='modal-close-btn' class='modal-close-btn'><strong>X</strong></button>
+      <div class='modal-info-container'>
+        <img class='modal-img' src='https://placehold.it/125x125' alt='profile picture'>
+        <h3 id='name' class='modal-name cap'>name</h3>
+        <p class='modal-text'>email</p>
+        <p class='modal-text cap'>city</p>
         <hr>
-        <p class="modal-text">(555) 555-5555</p>
-        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-        <p class="modal-text">Birthday: 10/21/2015</p>
+        <p class='modal-text'>(555) 555-5555</p>
+        <p class='modal-text'>123 Portland Ave., Portland, OR 97204</p>
+        <p class='modal-text'>Birthday: 10/21/2015</p>
       </div>
     </div>
   `;
@@ -84,9 +87,9 @@ function generateModal() {
 // allows user to navigate to the previous and next employee modal
 function generateModalButtons() {
  const modButton = `
-   <div class="modal-btn-container">
-       <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-       <button type="button" id="modal-next" class="modal-next btn">Next</button>
+   <div class='modal-btn-container'>
+       <button type='button' id='modal-prev' class='modal-prev btn'>Prev</button>
+       <button type='button' id='modal-next' class='modal-next btn'>Next</button>
    </div>
  `;
 }
